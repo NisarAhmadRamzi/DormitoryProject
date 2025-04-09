249d269b541d8e63ceeb5bc1585fc527c612b087
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-04-09 10:01:38 -0700

    stted the button

--- resources/js/pages/Rooms.jsx
+++ resources/js/pages/Rooms.jsx
@@ -1,28 +1,29 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+import { Button } from 'react-bootstrap'
+import CreateRoomForm from '../features/rooms/CreateRoomForm'
 import RoomTable from '../features/rooms/RoomTable'
 import { getCabins } from '../services/apiCabins'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
 
 function Rooms() {
+  const [showForm, setShowForm] = useState(false)
   useEffect(function () {
     getCabins().then((data) => console.log(data))
   }, [])
   return (
     <>
-      <Row
-        type="horizontal"
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          width: '100%',
-        }}
-      >
+      <Row type="horizontal">
         <Heading as="h1">All Rooms</Heading>
-        <p style={{ marginLeft: 'auto' }}>Filter/Sort</p>
+        <p>Filter/Sort</p>
       </Row>
       <Row>
         <RoomTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && CreateRoomForm}
       </Row>
     </>
   )
