a44d78738e11fbbcb3cd987201ec349d8fda14f7
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-04-09 00:50:30 -0700

    the wieght of sidebar items corrected

--- resources/js/ui/SideBar.jsx
+++ resources/js/ui/SideBar.jsx
@@ -1,17 +1,19 @@
-import Logo from "./Logo";
-import MainNav from "./MainNav";
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
+import Logo from './Logo'
+import MainNav from './MainNav'
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
-  padding: 3.2rem 2.4rem;
+  /* padding: 3.2rem 1.5rem; */
+  padding: 3.2rem 1.5rem 2rem 0;
   border-right: 1px solid var(--color-gray-100);
   grid-row: 1 /-1;
   display: flex;
   flex-direction: column;
   gap: 3.2rem;
-`;
+  width: 270px;
+`
 
 const Sidebar = () => {
   return (
@@ -19,7 +21,7 @@ const Sidebar = () => {
       <Logo />
       <MainNav />
     </StyledSidebar>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
