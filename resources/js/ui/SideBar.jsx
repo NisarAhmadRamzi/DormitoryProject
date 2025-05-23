029d4d8c315ed9c10d396dd82fba7880aa007cf9
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-05-23 08:30:29 -0700

    changes in sidebar

--- resources/js/ui/SideBar.jsx
+++ resources/js/ui/SideBar.jsx
@@ -1,19 +1,72 @@
-import React from 'react'
-import styled from 'styled-components'
 import Logo from '../ui/Logo'
 import MainNav from './MainNav'
+import styled from 'styled-components'
 
+// const StyledSidebar = styled.aside`
+//   background-color: var(--color-grey-0);
+//   padding: 3.2rem 2.4rem;
+//   border-right: 1px solid var(--color-grey-100);
+//   overflow: scroll;
+
+//   grid-row: 1 / -1;
+//   display: flex;
+//   flex-direction: column;
+//   gap: 3.2rem;
+// `
+
+//2
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
-  overflow: scroll;
+  overflow-y: auto;
 
   grid-row: 1 / -1;
   display: flex;
   flex-direction: column;
   gap: 3.2rem;
+
+  /* Scrollbar styling */
+  scrollbar-width: thin; /* Firefox */
+  scrollbar-color: var(--color-grey-400) transparent;
+
+  /* Chrome, Edge, Safari scrollbar */
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--color-grey-400);
+    /* border-radius: 0; <- remove rounded corners */
+    /* border: none; */
+  }
 `
+
+//v3
+// const StyledSidebar = styled.aside`
+//   background-color: var(--color-grey-0);
+//   padding: 3.2rem 2.4rem;
+//   border-right: 1px solid var(--color-grey-100);
+
+//   /* Enable vertical scrolling */
+//   overflow-y: auto;
+
+//   /* Hide scrollbar (cross-browser) */
+//   scrollbar-width: none; /* Firefox */
+//   -ms-overflow-style: none; /* Internet Explorer 10+ */
+//   &::-webkit-scrollbar {
+//     display: none; /* Chrome, Safari, Opera */
+//   }
+
+//   grid-row: 1 / -1;
+//   display: flex;
+//   flex-direction: column;
+//   gap: 3.2rem;
+// `
 
 function Sidebar() {
   return (
