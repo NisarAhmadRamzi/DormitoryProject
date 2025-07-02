327a772f780f796a23c8cada63403e0dba301e6e
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-07-01 23:47:51 -0700

    translated the Rooms compo

--- resources/js/pages/Rooms.jsx
+++ resources/js/pages/Rooms.jsx
@@ -1,14 +1,17 @@
+import { useTranslation } from 'react-i18next'
 import AddRoom from '../features/rooms/AddRoom'
 import RoomTable from '../features/rooms/RoomTable'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
 
 function Rooms() {
+  const { t } = useTranslation()
+
   return (
     <>
       <Row>
         <Heading as="h1" style={{ textAlign: 'center' }}>
-          Rooms
+          {t('rooms')}
         </Heading>
         <RoomTable />
         <AddRoom />
