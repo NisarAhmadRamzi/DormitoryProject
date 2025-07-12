883356c5a6a5bcce450b059bb3dd7c2c746427ff
NisarAhmadRamzi <nisarahmadramzi360@gmail.com>
2025-07-12 15:26:23 +0430

    backend some changes

--- app/Http/Controllers/backend/SupportController.php
+++ app/Http/Controllers/backend/SupportController.php
@@ -10,15 +10,15 @@ use Illuminate\Support\Facades\Artisan;
 
 class SupportController extends Controller
 {
-    // public function __construct()
-    // {
-    //     Artisan::call('permission:cache-reset');
-    //     $this->middleware('permission:all supports')->only(['index']);
-    //     $this->middleware('permission:view support')->only(['show']);
-    //     $this->middleware('permission:create support')->only(['store']);
-    //     $this->middleware('permission:edit support')->only(['update']);
-    //     $this->middleware('permission:delete support')->only(['destroy']);
-    // }
+    public function __construct()
+    {
+        Artisan::call('permission:cache-reset');
+        $this->middleware('permission:all supports')->only(['index']);
+        $this->middleware('permission:view support')->only(['show']);
+        $this->middleware('permission:create support')->only(['store']);
+        $this->middleware('permission:edit support')->only(['update']);
+        $this->middleware('permission:delete support')->only(['destroy']);
+    }
     // List all supports
     public function index()
     {
